14f922b8a944b5d6bcb882c213c991a98a1d2b9d
Anton Zaitsev <zaitsev.anton@outlook.com>
2022-09-04 18:50:59 +0200

    [FEATURE] Add new game creation handling (#11)
    * [TASK] game_service accepts new game post requests
    
    * [TASK] Change game creation endpoint

--- backend/game_service/src/app.js
+++ backend/game_service/src/app.js
@@ -1,18 +1,62 @@
 import express from "express"
-import amqplib from "amqplib"
-
 const app = express()
+import cors from "cors"
+import { nanoid } from "nanoid"
+import { createServer } from "http"
+import { Server } from "socket.io"
 
-const PORT = process.env.PORT || 8000
-
-app.use(express.json())
-
-// TODO: AMQP (RabbitMQ) connection
-
-app.get("/", (req, res) => {
-  res.send("game_service")
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 })
 
-app.listen(PORT, () => {
+app.use(cors())
+app.use(express.json())
+
+const games = []
+
+class Game {
+  constructor() {
+    this.id = nanoid(10)
+    this.players = []
+  }
+
+  addPlayer(player) {
+    this.players.push(player)
+  }
+
+  removePlayer(player) {
+    this.players = this.players.filter((p) => p.id !== player.id)
+  }
+
+  getPlayers() {
+    return this.players
+  }
+}
+
+class Player {
+  constructor(name) {
+    this.id = nanoid(10)
+    this.name = name
+  }
+}
+
+app.post("/api/game", (req, res) => {
+  const name = req.body.name || "Anonymous"
+  const game = new Game()
+  const player = new Player(name)
+
+  game.addPlayer(player)
+
+  games.push(game)
+
+  res.json(game)
+})
+
+const PORT = process.env.PORT || 8000
+httpServer.listen(PORT, () => {
   console.log(`game_service listening on port ${PORT}!`)
 })
